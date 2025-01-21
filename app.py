e2c8e1347da1f16149f9a9b6c454d824e22a9502
245 <54387747+uobasi@users.noreply.github.com>
2025-01-21 13:25:18 -0500

    .

--- app.py
+++ app.py
@@ -2344,7 +2344,7 @@ from google.api_core.exceptions import NotFound
 from scipy.signal import filtfilt, butter, lfilter
 from dash import Dash, dcc, html, Input, Output, callback, State
 initial_inter = 1000000  # Initial interval #210000#250000#80001
-subsequent_inter = 60000  # Subsequent interval
+subsequent_inter = 80000  # Subsequent interval
 app = Dash()
 app.title = "Envisage"
 app.layout = html.Div([
