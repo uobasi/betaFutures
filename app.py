3a29b03777445274882ab6cf1e4864d2412e007a
245 <54387747+uobasi@users.noreply.github.com>
2025-04-01 11:46:11 -0400

    .

--- app.py
+++ app.py
@@ -1439,13 +1439,13 @@ def plotChart(df, lst2, num1, num2, x_fake, df_dx,  stockName='', troPerCandle:l
             '''
             
 
-        if '02:00:00' in df['time'].values:
-            fig.add_vline(x=df[df['time'] == '02:00:00'].index[0], line_width=1, line_dash="dash", line_color="green", annotation_text='London Open', annotation_position='top left', row=1, col=1)
+        if '03:00:00' in df['time'].values:
+            fig.add_vline(x=df[df['time'] == '03:00:00'].index[0], line_width=1, line_dash="dash", line_color="green", annotation_text='London Open', annotation_position='top left', row=1, col=1)
             
             fig.add_trace(go.Scatter(x=df['time'],
-                                    y= [df['open'][df[df['time'] == '02:00:00'].index[0]]]*len(df['time']) ,
+                                    y= [df['close'][df[df['time'] == '02:50:00'].index[0]]]*len(df['time']) ,
                                     line_color='black',
-                                    text = str(df['open'][df[df['time'] == '02:00:00'].index[0]]),
+                                    text = str(df['close'][df[df['time'] == '02:50:00'].index[0]]),
                                     textposition="bottom left",
                                     name='London Open',
                                     showlegend=False,
@@ -1455,9 +1455,9 @@ def plotChart(df, lst2, num1, num2, x_fake, df_dx,  stockName='', troPerCandle:l
             
     
         if '04:00:00' in df['time'].values:
-            fig.add_vline(x=df[df['time'] == '04:00:00'].index[0], line_width=1, line_dash="dash", line_color="red", annotation_text='Toyko Close', annotation_position='top left', row=1, col=1)
+            fig.add_vline(x=df[df['time'] == '03:50:00'].index[0], line_width=1, line_dash="dash", line_color="red", annotation_text='Toyko Close', annotation_position='top left', row=1, col=1)
             
-            tempDf = df.loc[df[df['time'] == opstr].index[0]:df[df['time'] == '04:00:00'].index[0]]
+            tempDf = df.loc[df[df['time'] == opstr].index[0]:df[df['time'] == '03:50:00'].index[0]]
             max_high = tempDf['high'].max()
             min_low = tempDf['low'].min()
             fig.add_trace(go.Scatter(x=df['time'],
@@ -1501,9 +1501,9 @@ def plotChart(df, lst2, num1, num2, x_fake, df_dx,  stockName='', troPerCandle:l
             fig.add_vline(x=df[df['time'] == '08:00:00'].index[0], line_width=1, line_dash="dash", line_color="green", annotation_text='NewYork Open', annotation_position='top left', row=1, col=1)
             
             fig.add_trace(go.Scatter(x=df['time'],
-                                    y= [df['open'][df[df['time'] == '08:00:00'].index[0]]]*len(df['time']) ,
+                                    y= [df['close'][df[df['time'] == '07:50:00'].index[0]]]*len(df['time']) ,
                                     line_color='black',
-                                    text = str(df['open'][df[df['time'] == '08:00:00'].index[0]]),
+                                    text = str(df['close'][df[df['time'] == '07:50:00'].index[0]]),
                                     textposition="bottom left",
                                     name='NewYork Open',
                                     showlegend=False,
@@ -1512,10 +1512,10 @@ def plotChart(df, lst2, num1, num2, x_fake, df_dx,  stockName='', troPerCandle:l
                                     ))
             
     
-        if '11:00:00' in df['time'].values:
-            fig.add_vline(x=df[df['time'] == '11:00:00'].index[0], line_width=1, line_dash="dash", line_color="red", annotation_text='London Close', annotation_position='top left', row=1, col=1)
+        if '11:30:00' in df['time'].values:
+            fig.add_vline(x=df[df['time'] == '11:30:00'].index[0], line_width=1, line_dash="dash", line_color="red", annotation_text='London Close', annotation_position='top left', row=1, col=1)
             
-            tempDf = df.loc[df[df['time'] == '02:00:00'].index[0]:df[df['time'] == '11:00:00'].index[0]]
+            tempDf = df.loc[df[df['time'] == '02:00:00'].index[0]:df[df['time'] == '11:20:00'].index[0]]
             max_high = tempDf['high'].max()
             min_low = tempDf['low'].min()
             fig.add_trace(go.Scatter(x=df['time'],
